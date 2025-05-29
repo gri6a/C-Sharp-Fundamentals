010a3ec09e4aa15cde05ad908bdbc3507e5c7012
gri6a <gw2803@gmail.com>
2025-05-29 02:56:26 +0100

    intro to data types

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// .dll .exe
 using System;
 
 namespace ConsoleApp1
 {
     class Program
-    {
+    { // public means that this method is accessible from another classes
+        // classes cannot go into Main block
+        public void getData() // void means that this method does not return anything
+        {
+            Console.WriteLine("I'm inside the method.");
+        }
+
         static void Main(string[] args)
         {
+            // to call getData() method we have to create an object
+            // based on Program class
+            Program p = new Program();
+            p.getData();
+
+            // int (whole numbers) and String are static data types
             Console.WriteLine("Hello World");
+            int a = 10;
+            double c = 11.22;
+            Console.WriteLine("Number is " + a);
+            string name = "Raf";
+            Console.WriteLine("Name is " + name);
+            string name2 = "Betty";
+            Console.WriteLine($"2nd name is {name2}");
+
+            // var is dynamic data type - use when you don't know what data type you are dealing with
+            var age = 30;
+            var age2 = "30";
+            Console.WriteLine($"age is {age}");
+            Console.WriteLine($"age2 is {age2}");
+            // age = "hello" not allowed
+            // dynamic keyword for dynamic data type
+            dynamic height = 13.2; // if dynamic type you can change the value of height to different data types
+            Console.WriteLine($"Value of height as double {height}");
+            height = "hello";
+            Console.WriteLine($"Value of height as String {height}");
         }
     }
 }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dff7aaf507c1627dab16c6b0d0f49ef973cb064")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
